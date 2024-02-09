867c09aa74b70640c3a00ddf98c6e8266b6dc9c1
Karim Dilling <ktk.dilling@freenet.de>
2024-02-09 18:22:59 +0100

    Remove disconnected clients from connection list

--- main.go
+++ main.go
@@ -23,26 +23,43 @@ func main() {
 		}
 		conns = append(conns, conn)
 		log.Printf("Accepted connection from %v\n", conn.RemoteAddr())
-		messages <- Message{conn, ""}
 		go receiveMessages(conn, messages)
 	}
 }
 
+type MessageType int
+
+const (
+	ClientDisconnected MessageType = iota
+	ClientMessage
+)
+
 type Message struct {
 	conn    net.Conn
+	msgType MessageType
 	content string
 }
 
 func sendMessages(messages chan Message, conns *[]net.Conn) {
 	for {
 		msg := <-messages
+		if msg.msgType == ClientDisconnected {
+			disconnectedAddr := msg.conn.RemoteAddr()
+			for i := 0; i < len(*conns); i++ {
+				if (*conns)[i].RemoteAddr() == disconnectedAddr {
+					*conns = append((*conns)[:i], (*conns)[i+1:]...)
+					i--
+				}
+			}
+			log.Printf("Client with address %s disconnected\n", msg.conn.RemoteAddr())
+		}
 		for _, conn := range *conns {
 			if conn == msg.conn {
 				continue
 			}
 			_, err := conn.Write([]byte(msg.content))
 			if err != nil {
-				log.Printf("Could not send message to %s: %s", conn.RemoteAddr(), err)
+				log.Printf("Could not send message to %s: %s\n", conn.RemoteAddr(), err)
 			}
 		}
 	}
@@ -51,14 +68,15 @@ func sendMessages(messages chan Message, conns *[]net.Conn) {
 func receiveMessages(conn net.Conn, clientMsg chan Message) {
 	defer conn.Close()
 
+	buffer := make([]byte, 80)
 	for {
-		buffer := make([]byte, 200)
 		n, err := conn.Read(buffer)
 		if err != nil {
 			conn.Close()
+			clientMsg <- Message{conn, ClientDisconnected, ""}
 			return
 		}
 		content := string(buffer[0:n])
-		clientMsg <- Message{conn, content}
+		clientMsg <- Message{conn, ClientMessage, content}
 	}
 }
